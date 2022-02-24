93dc8f4457da40c0a9ff325dd9716ff31d0fa87c
Michael Buffone <m_buffone@hotmail.com>
2022-02-24 16:45:25 -0500

    refactor to export to index.js and remove console.log for callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,24 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  //changing it back to reguar javascript
-  const data = JSON.parse(body);
-  //check if theres a error from searching the site
-  if (error) {
-    console.log('Error');
-  //if the breed name doestn exist will return 0 for length
-  } else if (data.length === 0) {
-    console.log(`Bread ${breed} not found`);
-  //return the breeds description
-  } else {
-    console.log(data[0].description);
-  }
+const fetchBreedDescription = ((breed, callback) => {
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    //changing it back to reguar javascript
+    const data = JSON.parse(body);
+    //check if theres a error from searching the site
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    //if the breed name doesn't exist will return 0 for length
+    if (data.length === 0) {
+      callback(`Bread ${breed} not found!`, null);
+    //return the breeds description
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+
 });
+
+module.exports =  { fetchBreedDescription };
